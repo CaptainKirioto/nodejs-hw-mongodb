0d75a22af9edb1b78b6b40da2cd80224ed4b1ea3
CaptainKirioto <mrg.zhi@gmail.com>
2024-11-17 12:19:03 +0100

    auth

---
+++ src/routers/auth.js
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import validateBody from '../utils/validateBody.js';
+import { authRegisterSchema, authLoginSchema } from '../validation/auth.js';
+
+const authRouter = Router();
+
+authRouter.post('/register', validateBody(authRegisterSchema), ctrlWrapper());
+
+authRouter.post('/login', validateBody(authLoginSchema), ctrlWrapper());
+
+export default authRouter;
